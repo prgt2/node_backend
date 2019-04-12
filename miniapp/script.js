2ff763eafa377a421a50415a06938bb5ad3f6441
einverv <excelall@excelall.pl>
2019-04-12 12:41:37 +0200

    added timeouts

--- miniapp/script.js
+++ miniapp/script.js
@@ -1,19 +1,23 @@
 function script(){
+    var lista_kooperantow
     $('#wybierz_kooperanta').on('click', function() {
         if ($(this).attr('class') !== 'clicked') {
             console.log($(this).attr('class'))
             $.get('/api/kooperanci', (data) => {
-                //console.log(data)
                 $(data).each(function() {
                     $('#wybierz_kooperanta').append(`<option value="${this.id}" class="lista_kooperantow" id="${this.id}">${this.name}</option>`)
-                    //console.log(this)
                 })
             })
             $(this).toggleClass('clicked')
-        } else {
-            console.log('halo bylo klikniete', $(this).attr())
-        }    
+            setTimeout(a, 100)
+            setTimeout(b, 200)
+        }   
     })
+    function a() {
+            lista_kooperantow = $('#wybierz_kooperanta').children()
+            console.log(lista_kooperantow)
+    }
+    
     let wiersz_tabeli = `<tr class="products_row">
     <td class="lp">${this.id}</td>
     <td class="product_name">${this.nazwa}</td>
@@ -27,8 +31,8 @@ function script(){
         $(data).each(function() {
             $('#table_header').append('wiersz_tabeli')
         })
-    }) */
-    let lista_kooperantów = $('.lista_kooperantow')
+    }) 
+    var lista_kooperantow = $('.lista_kooperantow')
     let go_to_clients_site = function() {
         if ($(this).attr('class') !== 'clicked') {
             $.get('/api/produkta', function(data) {
@@ -41,9 +45,23 @@ function script(){
             console.log($(this).attr('id'))
         }
         $(this).toggleClass('clicked')
-    } 
-    $(lista_kooperantów).each(function() {
-        $(this).on('click', go_to_clients_site())
-    })
-
+    }  */
+    function b() {
+        $(lista_kooperantow).each(function() {
+            $(this).on('click', function() {
+                console.log('klikles w kooperanta')
+                if ($(this).attr('class') !== 'clicked') {
+                    $.get('/api/produkta', function(data) {
+                        $(data).each(function() {
+                            $('#table_header').append('wiersz_tabeli')
+                        })
+                    })
+                    //$.get(`/api/kooperanci/:${$(this).attr('id')}`, function(data) {
+                    //})
+                    console.log($(this).attr('id'))
+                }
+                $(this).toggleClass('clicked')
+            }, this )
+        })
+    }
 }
